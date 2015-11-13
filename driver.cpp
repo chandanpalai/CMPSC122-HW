705010d54feb8e2a282088ec779b9fcf8bc9545d
Nathan Smith <n.smith167@gmail.com>
2015-11-13 15:25:01 -0500

    Awaiting final confirmation

--- driver.cpp
+++ driver.cpp
@@ -10,13 +10,13 @@ using namespace std;
 int main()
 {
    Scheduler *scheds[] = 	    // make some process schedulers
-	{ new FCFS(), new RoundRobin(), new Priority(), new Preempt() };
+	{ new FCFS(), new RoundRobin(), new Priority(), new Preempt(), new SRT() };
    Process *tasks[] = 		    // 4 processes
 	{ new Interact(0), new Download(1), new Computation(2), new Download(3), new Computation(4), new Computation(5), new Computation(6), new Interact(7)};
    int arrival[] = {0, 100, 250, 375, 400, 430, 490, 500};   // arrive at these times
 
 
-   for (int i = 0; i < 4; i++)
+   for (int i = 4; i < 5; i++)
    {
 	   scheds[i]->runScheduler(tasks, arrival, 8);
 	   displayHistory(tasks, 8, 0, 7000);
--- scheduler.h
+++ scheduler.h
@@ -24,7 +24,7 @@ class Scheduler
 	{
 	    readySet.pushBack( procId, 0, 'X');
 	}
-	void chooseProcess( int &procId )
+	virtual void chooseProcess( int &procId )
 	{
 	    char hold;
 	    readySet.popFront( procId, hold );
@@ -37,7 +37,7 @@ class Scheduler
 	{
 	    return readySet.empty();
 	}
-        void runScheduler( Process* [], int[], int );
+    virtual void runScheduler( Process* [], int[], int );
 };
 
 class FCFS : public Scheduler  
@@ -104,7 +104,7 @@ class SRT :public Scheduler
 		int last_insertion = -1; //index of the most recently inserted value
 		Process *temp_storage; //for swapping
 		int final_loc; //ending location of a value
-		int tracking_index; //index of the child of final_loc as it progresses towards its final index
+		int tracking_index; //index of that "follows" the final_loc index as the process stored at final_loc moves towards its ending location
 	public:
 		SRT() { name = "SRT"; }
 
@@ -112,21 +112,23 @@ class SRT :public Scheduler
 		void runScheduler(Process* tasks[], int arrival[], int size)
 		{
 			procs = tasks;
+			readySet[0] = NULL;
 			Scheduler::runScheduler(tasks, arrival, size);
 		}
 		void addProcess(int procId)
 		{
 			last_insertion++;
-			readySet[last_insertion] = ;
+			readySet[last_insertion] = procs[procId];
 			final_loc = last_insertion;
 			tracking_index = last_insertion;
-			if (last_insertion > 0)
+			if (last_insertion > 0) //no moving necessary if process is inserted at the root
 			{
-				while (tasks[(readySet[((final_loc - 1) / 2)]]->dd > procId))
+				//moves child to parent as long as child is not root and parent has more remaining time than child
+				while (final_loc != 0 && (readySet[((final_loc - 1) / 2)]->getRemainingTime() > readySet[final_loc]->getRemainingTime()))
 				{
 					final_loc = ((final_loc - 1) / 2);
 					temp_storage = readySet[final_loc];
-					readySet[final_loc] = procId;
+					readySet[final_loc] = readySet[tracking_index];
 					readySet[tracking_index] = temp_storage;
 					tracking_index = ((tracking_index - 1) / 2);
 				}
@@ -135,13 +137,46 @@ class SRT :public Scheduler
 		}
 		void chooseProcess(int &procId)
 		{
-			procId = readySet[0];
-			final_loc = 0;
-			readySet[0] = readySet[last_insertion];
-			readySet[last_insertion] = -1;
-			while (readySet[final_loc] > )
+			procId = readySet[0]->getId();
+			if (last_insertion > 0) //otherwise there is nothing in the array to move
+			{
+				final_loc = 0;
+				readySet[0] = readySet[last_insertion]; //most recent insertion moved to root
+				//runs until there are no more children to swap with or until the parent has less remaining time than both children
+				while (last_insertion >= (2 * final_loc) + 2 && final_loc <= 8 && (readySet[final_loc]->getRemainingTime() > readySet[(2 * final_loc) + 1]->getRemainingTime() || readySet[final_loc]->getRemainingTime() > readySet[(2 * final_loc) + 2]->getRemainingTime()))
+				{
+					if (readySet[final_loc]->getRemainingTime() > readySet[(2 * final_loc) + 1]->getRemainingTime() && readySet[(2 * final_loc) + 1]->getRemainingTime() < readySet[(2 * final_loc) + 2]->getRemainingTime())
+					{
+						tracking_index = final_loc;
+						final_loc = (final_loc * 2) + 1;
+						temp_storage = readySet[tracking_index];
+						readySet[tracking_index] = readySet[final_loc];
+						readySet[final_loc] = temp_storage;
+					}
+					else if (readySet[final_loc]->getRemainingTime() > readySet[(2 * final_loc) + 2]->getRemainingTime() && readySet[(2 * final_loc) + 1]->getRemainingTime() > readySet[(2 * final_loc) + 2]->getRemainingTime())
+					{
+						tracking_index = final_loc;
+						final_loc = (final_loc * 2) + 2;
+						temp_storage = readySet[tracking_index];
+						readySet[tracking_index] = readySet[final_loc];
+						readySet[final_loc] = temp_storage;
+					}
+				}
+				if (final_loc == 9 && last_insertion >= 19)
+				{
+					if (readySet[final_loc]->getRemainingTime() > readySet[(2 * final_loc) + 1]->getRemainingTime() && readySet[(2 * final_loc) + 1]->getRemainingTime() < readySet[(2 * final_loc) + 2]->getRemainingTime())
+					{
+						tracking_index = final_loc;
+						final_loc = (final_loc * 2) + 1;
+						temp_storage = readySet[tracking_index];
+						readySet[tracking_index] = readySet[final_loc];
+						readySet[final_loc] = temp_storage;
+					}
+				}
+			}
+			readySet[last_insertion] = NULL; //most recent insertion removed
+			last_insertion--;	
 		}
-		//modify this- should function similarly to preemptive priority, but using remaining time
 		int allowance()
 		{
 			if (future.empty()) //nothing of higher priority will be added to readySet
@@ -150,14 +185,14 @@ class SRT :public Scheduler
 				return future.leadTime() - clock;
 			else if (noneReady())
 				return 100000;
-			else if ((future.begin().re() > readySet.begin().process()) && (future.leadTime() - clock < 100000)) //a process of higher priority than the next item in readySet will be available before current execution is finished
+			else if ((procs[future.begin().process()]->getRemainingTime() < readySet[0]->getRemainingTime()) && (future.leadTime() - clock < 100000)) //a process of higher priority than the next item in readySet will be available before current execution is finished
 				return future.leadTime() - clock;
 			else //default allowance of Priority scheduler
 				return 100000;
 		}
 		bool noneReady()
 		{
-			return readySet[0] == -1;
+			return readySet[0] == NULL;
 		}
-		void runScheduler(Process*[], int[], int);
+		//void runScheduler(Process*[], int[], int);
 };
